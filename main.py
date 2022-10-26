fd2b81b1c66ee819e21596ad4ef34aa995a8b090
Peter Harrison <89763136+peter-harrison-pch@users.noreply.github.com>
2022-10-26 02:07:01 +0000

    multiple valid options wants alpahbetical/ansii?

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ names = input().split()
 
 naiveminmax = [names[0], names[0]]
 for name in names:
-    if len(name) <= len(naiveminmax[0]):
+    if len(name) <= len(naiveminmax[0]) and name[0] < naiveminmax[0][0]:
         naiveminmax[0] = name
         continue
-    if len(name) >= len(naiveminmax[1]):
+    if len(name) >= len(naiveminmax[1]) and name[0] < naiveminmax[1][0]:
         naiveminmax[1] = name
 
 print(*naiveminmax)
+
+#print("a" < "b")
+#print("Z" < "z")
+
+# so wants ansii alphabetical ordering
