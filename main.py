cf27109b6c5ac5fdae2443f6147388a96056893c
Peter Harrison <89763136+peter-harrison-pch@users.noreply.github.com>
2022-10-26 02:16:58 +0000

    fix alphabetical ordering

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ names = input().split()
 
 naiveminmax = [names[0], names[0]]
 for name in names:
-    if len(name) <= len(naiveminmax[0]) and name[0] < naiveminmax[0][0]:
+    if len(name) < len(naiveminmax[0]):
         naiveminmax[0] = name
         continue
-    if len(name) >= len(naiveminmax[1]) and name[0] < naiveminmax[1][0]:
+    if len(name) > len(naiveminmax[1]) and name[0] < naiveminmax[1][0]:
+        naiveminmax[1] = name
+    elif len(name) == len(naiveminmax[0]) and name < naiveminmax[0]:
+        naiveminmax[0] = name
+    elif len(name) == len(naiveminmax[1]) and name < naiveminmax[1]:
         naiveminmax[1] = name
 
 print(*naiveminmax)
