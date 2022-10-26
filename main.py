ec70b4e8374883e659d393f4eb161efb963a6f75
Peter Harrison <89763136+peter-harrison-pch@users.noreply.github.com>
2022-10-26 01:51:35 +0000

    basic loop instead of library function

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # ******************************
 # Make your Code
 # ******************************
+names = input("list of names: ")
+
+naiveminmax = [names[0], names[0]]
+for name in names:
+    if len(name) < len(naiveminmax[0]):
+        naiveminmax[0] = name
+        continue
+    if len(name) > len(naiveminmax[1]):
+        naiveminmax[1] = name
+
+print(*naiveminmax)
