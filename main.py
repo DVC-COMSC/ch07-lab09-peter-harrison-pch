2ec98639e3ae3141e447f9c24d4d76762396b5ba
Peter Harrison <89763136+peter-harrison-pch@users.noreply.github.com>
2022-10-26 01:57:07 +0000

    >= instead to boot intial first/last with same len

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ names = input().split()
 
 naiveminmax = [names[0], names[0]]
 for name in names:
-    if len(name) < len(naiveminmax[0]):
+    if len(name) <= len(naiveminmax[0]):
         naiveminmax[0] = name
         continue
-    if len(name) > len(naiveminmax[1]):
+    if len(name) >= len(naiveminmax[1]):
         naiveminmax[1] = name
 
 print(*naiveminmax)
